ce8cd36b594c894a2f8345d948b742e790c45e6f
Muhammad Waleed Khan <waleedjaved911@gmail.com>
2025-11-11 20:05:29 +0500

    using the dbg!

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,16 +1,20 @@
+#[derive(Debug)]
 struct Rectangle {
     height: u32,
     width: u32,
 }
 
 fn main() {
+    let scale: u32 = 2;
     let rect1: Rectangle = Rectangle {
-        height: 3,
+        height: dbg!(3 * scale),
         width: 8,
     };
 
     let value = area(&rect1);
     println!("The area of the rectangle is {} square pixels", value);
+    dbg!("rect1 has values {:?}", &rect1); //takes ownership so an explicit reference is provided
+    println!("rect1 has values {:#?}", rect1); //takes reference
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
